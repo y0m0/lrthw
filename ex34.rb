6ea1a7f728f76d84c654587437ff54a6a733ae11
y0m0 <lixi.daniele@gmail.com>
2017-04-13 21:01:08 +0200

    ex34

---
+++ ex34.rb
@@ -0,0 +1,11 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+puts "The animal at 1 is ruby and is == #{animals[1]}"
+puts "The third (3rd) animal is peackock and is == #{animals[2]}"
+puts "The first (1st) animal is bear and is == #{animals[0]}"
+puts "The animal at 3 is kangaroo and is == #{animals[3]}"
+puts "The fifth (5th) animal is whale and is == #{animals[4]}"
+puts "The animal at 2 is peackock and is == #{animals[2]}"
+puts "The sixth (6th) animal platypus and is == #{animals[5]}"
+puts "The animal at 4 is whale and is == #{animals[4]}"
