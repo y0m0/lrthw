11c62a98dc1c121992eb04b05f56fce856b74858
y0m0 <lixi.daniele@gmail.com>
2017-04-15 12:52:07 +0200

    ex42.rb

---
+++ ex42.rb
@@ -0,0 +1,105 @@
+## Animal is-a object look at the extra credit
+class Animal
+
+  def speak(sound)
+    puts sound
+  end
+end
+
+## Dog is-a Animal
+class Dog < Animal
+
+  def initialize(name)
+    ## Dog has-a name
+    @name = name
+  end
+end
+
+## Cat is-a Animal
+class Cat < Animal
+
+  def initialize(name)
+    ## cat has-a name
+    @name = name
+  end
+end
+
+## Person is-a object
+class Person
+
+  def initialize(name)
+    ## Person has-a name
+    @name = name
+
+    ## Person has-a pet of some kind
+    @pet = nil
+  end
+
+  attr_accessor :pet
+end
+
+## Employee is-a Person
+class Employee < Person
+
+  def initialize(name, salary)
+    ## Employee has-a Person name
+    super(name)
+    ## Employee has-a salary
+    @salary = salary
+  end
+
+end
+
+## Fish is-a object
+class Fish
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def swim
+    "swims away!"
+  end
+end
+
+## Salmon is-a Fish
+class Salmon < Fish
+end
+
+## Halibut is-a Fish
+class Halibut < Fish
+end
+
+
+## rover is-a Dog
+rover = Dog.new("Rover")
+
+## satan is-a Cat
+satan = Cat.new("Satan")
+
+## mary is-a Person
+mary = Person.new("Mary")
+
+## mary has-a pet satan
+mary.pet = satan
+
+## frank is-a Employee, frank has-a name frank and has-a salary 120000
+frank = Employee.new("Frank", 120000)
+
+## frank has-a pet rover
+frank.pet = rover
+
+## flipper is-a Fish
+flipper = Fish.new("Flipper")
+
+## crouse is-a Salmon
+crouse = Salmon.new("crouse")
+
+## harry is-a Halibut
+harry = Halibut.new("harry")
+
+
+puts flipper.name + " " + flipper.swim
+rover.speak("bau bau!")
+satan.speak("miao miao!")
